1fd196bc669b16c86223a723c1810c81bf0dc3d3
victorseara <victorseara.me@gmail.com>
2025-06-23 00:34:20 -0300

    tests: add playwright and setup a basic test for the recommendations section

---
+++ e2e/cart-page.spec.ts
@@ -0,0 +1,30 @@
+import test, { expect } from "@playwright/test";
+import { Recommendations } from "./utils/recommendations";
+
+test.describe("Recommendation List", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/checkout/cart');
+    })
+
+    test("show recommendation items in a list", async ({ page }) => {
+        const recommendations = new Recommendations(page);
+        const items = await recommendations.getListItems();
+        
+        await recommendations.assertOnListItems(items);
+        expect(items).toHaveLength(Recommendations.DEFAULT_LIST_SIZE);
+    });
+
+    test("navigates to product's page when clicked", async ({ page }) => {
+        const recommendations = new Recommendations(page);
+        const items = await recommendations.getListItems();
+        const product = items[0];
+
+        await expect(product.link).toBeVisible();
+
+        const productLink = await product.link.getAttribute("href");
+        expect(productLink).toBeDefined();
+
+        await product.link.click();
+        await page.waitForURL(productLink!);
+    })
+})
